b910afbb35a6ca1fc1d8458750092093b0460eca
VovaDev777 <polvovater@gmail.com>
2023-12-29 17:10:34 +0100

    1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Profile.css'
+
+const Profile = ({username, tag, location, avatar, stats}) => {
+  return (
+    <div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+      className="avatar"
+      width={100}
+      height={100}
+    />
+    <p className="name">{username}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
+  </div>
+
+  <ul className="stats">
+    <li className='stats-item'>
+      <span className="label">Followers</span>
+      <span className="quantity">{stats.followers}</span>
+    </li>
+    <li className='stats-item'>
+      <span className="label">Views</span>
+      <span className="quantity">{stats.views}</span>
+    </li>
+    <li className='stats-item'>
+      <span className="label">Likes</span>
+      <span className="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+  );
+};
+
+export {Profile};
