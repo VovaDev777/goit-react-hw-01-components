fa1f2d651119cc465c8cc989c89ac21ea5a20827
VovaDev777 <polvovater@gmail.com>
2023-12-30 12:31:46 +0100

    1

--- src/components/Statistic/Statistics.jsx
+++ src/components/Statistic/Statistics.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './Statistics.css';
 
 
-const Statistics = ({stats}) => {
+const Statistics = ({title, stats}) => {
   return (
     <section className="statistics">
-
+      {title && <h2 className={title}>{title}</h2>}
   <ul className="stat-list">
     {stats.map(stat => (
         <li key = {stat.id} className="item">
